b7c0605d0f78f1396c463ef1933744b85313cfe5
afbenitez <af.benitez@uniandes.edu.co>
2021-10-09 04:35:43 -0500

    item service

---
+++ lib/services/item_s.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:trapp_flutter/models/city.dart';
+import 'package:trapp_flutter/models/item.dart';
+
+class ItemService {
+
+
+  final CollectionReference itemsCollection =
+  FirebaseFirestore.instance.collection('items');
+
+
+  List<Stream<Item>> getItems(List<DocumentReference> entities) {
+
+    List<Stream<Item>> retVal = [];
+    entities.forEach((element) {
+      retVal.add(element.snapshots().map((_itemsListFromSnapshot)));
+    });
+    return retVal;
+  }
+
+  Item _itemsListFromSnapshot(DocumentSnapshot snapshot) {
+    return Item(
+      uid: snapshot.id,
+      name: snapshot['name'],
+    );
+  }
+
+
+
+}
