5cf26aefff0592028f5e305c11b8a86e5d4b1a77
Adriana Trujillo <a.trujilloa1@uniandes.edu.co>
2021-10-26 16:23:38 -0500

    Authentication fixed and budget view

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -22,89 +22,91 @@ class Home extends StatelessWidget {
   }
 
   Widget _content(context) {
-    return Padding(
-        padding: const EdgeInsets.all(22),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const Center(
-                child: Text(
-                  'TRAPP',
-                  style: TextStyle(
-                    fontFamily: 'trapp',
-                    fontSize: 85,
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.all(22),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Center(
+                  child: Text(
+                    'TRAPP',
+                    style: TextStyle(
+                      fontFamily: 'trapp',
+                      fontSize: 85,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                  padding: const EdgeInsets.all(34),
-                  height: 500,
-                  decoration: BoxDecoration(
-                      color: Colors.grey.shade50,
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black,
-                          spreadRadius: 3,
-                          blurRadius: 10,
-                        )
-                      ]),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Positioned(
-                          child: Circulo(
-                            size: 200,
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                    padding: const EdgeInsets.all(34),
+                    height: 500,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade50,
+                        borderRadius: const BorderRadius.all(Radius.circular(20)),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            spreadRadius: 3,
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Positioned(
+                            child: Circulo(
+                              size: 200,
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        RaisedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        SignIn(toggleView: () {
-                                          // Navigator.pop(context);
-                                        })),
-                              );
-                            },
-                            color: const Color(0xff00AFB9),
-                            shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30))),
-                            child: const Center(
-                                child: Text(
-                              'Sign In',
-                              style: TextStyle(color: Color(0xffF3F9E3)),
-                            ))),
-                        const SizedBox(
-                          height: 0.9,
-                        ),
-                        RaisedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const SignUp()),
-                              );
-                            },
-                            color: const Color(0xffEDD83D),
-                            shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30))),
-                            child: const Center(
-                                child: Text(
-                              'Sign Up',
-                              style: TextStyle(color: Color(0xff481620)),
-                            )))
-                      ]))
-            ]));
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          RaisedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          SignIn(toggleView: () {
+                                            // Navigator.pop(context);
+                                          })),
+                                );
+                              },
+                              color: const Color(0xff00AFB9),
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(30))),
+                              child: const Center(
+                                  child: Text(
+                                'Sign In',
+                                style: TextStyle(color: Color(0xffF3F9E3)),
+                              ))),
+                          const SizedBox(
+                            height: 0.9,
+                          ),
+                          RaisedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const SignUp()),
+                                );
+                              },
+                              color: const Color(0xffEDD83D),
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(30))),
+                              child: const Center(
+                                  child: Text(
+                                'Sign Up',
+                                style: TextStyle(color: Color(0xff481620)),
+                              )))
+                        ]))
+              ])),
+    );
   }
 }
